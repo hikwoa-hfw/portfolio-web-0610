02a1228ffba74c8877dd206160edf400dd487d97
hikwoa-hfw <hibbanhfw@gmail.com>
2025-02-23 20:51:36 +0700

    chore: testimonials section margin and padding

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -2,14 +2,14 @@ import { Marquee3D } from "./ui/marqueedialog";
 
 const Testimonials = () => {
   return (
-    <div className="Yellow">
-      <div className="Yellow container mx-auto py-16">
+    <div className="Yellow h-screen">
+      <div className="Yellow container mx-auto py-8">
         <div className="my-8 max-w-[320px] rounded-xl border-[3px] border-black bg-[#fffcee] p-8 text-4xl font-semibold text-teal-700 shadow-custom">
           <p className="cursor-pointer transition-all duration-150 hover:underline hover:underline-offset-auto">
             Testimonials
           </p>
         </div>
-        <div className="flex mx-auto h-full py-20">
+        <div className="mt-24">
           <Marquee3D />
         </div>
       </div>
